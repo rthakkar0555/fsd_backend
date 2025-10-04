b5da0b0354e3f78c14896b1e0933961f3699774e
rthakkar0555 <rishithakkar0555@gmail.com>
2025-10-04 19:03:56 +0530

    done

--- app3.js
+++ app3.js
@@ -4,7 +4,7 @@ const app = express()
 const port = 4000
 
 app.get('/', (req,res) => {
-    res.send("Hello Prit!!!???")
+    res.send("Hello  Rishi!!!???")
 })
 
 app.get('/homesite', (req,res) => {
